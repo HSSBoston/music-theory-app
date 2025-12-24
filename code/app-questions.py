01e1f05f30e0346eb98ad55a9fc4246c43c354bc
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2025-12-24 18:44:30 -0500

    8app rev

---
+++ code/app-questions.py
@@ -0,0 +1,33 @@
+import random
+from music21 import *
+
+listNumOfAccidentals = [0,1,2,3,4,5,6,7]
+listSharpOrFlat = ["sharps", "flats"]
+
+accidentals = random.choice(listNumOfAccidentals)
+sharpOrFlat = random.choice(listSharpOrFlat)
+
+print("What is the name of the key signature with "+str(accidentals)+" "+sharpOrFlat)
+
+if sharpOrFlat == "sharps":
+    ks = key.KeySignature(accidentals);
+    key = ks.asKey()
+    print(key)
+else:
+    ks = key.KeySignature(-accidentals);
+    key = ks.asKey()
+    print(key)
+    
+print("What is the relative minor of "+str(key))
+
+print(key.relative)
+
+print("What is the parallel minor of "+str(key))
+
+print(key.parallel)
+
+
+
+
+
+
---
+++ code/ex1.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Streamlit Basics")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.text("This is a simple text")
+st.write("This is a write dimension")
+
+st.markdown("[Streamlit](https://www.streamlit.io)")
+st.markdown("https://www.streamlit.io")
+
+html_page = """
+<div style="background-color:blue;padding:50px">
+<p style="color:yellow;font-size:50px">Enjoy Streamlit!</p>
+</div>
+"""
+st.markdown(html_page, unsafe_allow_html=True)
+
+st.success("Success!")
+st.info("Inofrmation")
+st.warning("This is a warning!")
+st.error("This is an error!")
+
+img = Image.open("photo.png")
+st.image(img, width=300, caption="Packt Logo")
+
+st.video("https://www.youtube.com/watch?v=xAWDqdpOlu8")
+
+
+if st.button("Play"):
+    st.text("Hello world!")
+    
+if st.checkbox("Checkbox"):
+    st.text("Checkbox selected")
+    
+radio_but = st.radio("Your Selection", ["A","B"])
+if radio_but == "A":
+    st.info("You selected A")
+else:
+    st.info("You selected B")
+    
+city = st.selectbox("Your City", ["Bedford", "Lexington", "Burlington"])
+
+occupation = st.multiselect("Your Occupation", ["Programmer", "Data Scientist", "IT Consultant", "DBA"])
+
+Name = st.text_input("Your Name", "Write something...")
+st.text(Name)
+
+Age = st.number_input("Input a number")
+
+message = st.text_area("Your Messsage", "Write something...")
+
+select_val = st.slider("Select a Value", 1, 10)
+
+if st.button("Balloons"):
+    st.balloons()
